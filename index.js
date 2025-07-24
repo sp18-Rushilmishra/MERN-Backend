6e99a3190e1afcef8d85e3dfe479d52dd42410a6
Rushil Mishra <rushil4835@gmail.com>
2025-07-24 12:48:15 +0530

    adding the promises , async , readfile task

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const amount = 9;
+// const amount = 9;
 
-if(amount < 10) {
-    console.log('small nunmber')
-}else{
-    console.log('large number')
-}
+// if(amount < 10) {
+//     console.log('small nunmber')
+// }else{
+//     console.log('large number')
+// }
 
-console.log('hey it is my first node app!! ')
+// console.log('hey it is my first node app!! ')
+
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Express!');
+// });
+
+// app.listen(3000, () => {
+//   console.log('Server running on port 3000');
+// });
+
+// console.log(__dirname);
+
+// var fs = require('fs');
+// var os  = require('os');
+
+// var user = os.userInfo();
+// console.log(user);
+// console.log(user.username);
+
+// fs.appendFile('greeting.txt' , 'hi' + user.username + '!' , () => (
+//     console.log('file')
+// ) )
+
+
